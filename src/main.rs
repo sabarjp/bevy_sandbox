787ebb384948e2f9c205e3f37529dc68b80b2c28
Josh <jp@joshuapowers.com>
2022-07-27 06:07:57 -0500

    basic networking

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::borrow::Cow;
-use std::f32::consts::PI;
+use std::net::UdpSocket;
+use std::time::SystemTime;
 
 use bevy::app::App;
 use bevy::DefaultPlugins;
@@ -8,11 +8,10 @@ use bevy::ecs::system::lifetimeless::SRes;
 use bevy::ecs::system::SystemParamItem;
 use bevy::input::mouse::{MouseMotion, MouseWheel};
 use bevy::pbr::{AmbientLight, MaterialMeshBundle, MaterialPipeline, PointLightBundle, SpecializedMaterial};
-use bevy::prelude::{Assets, Camera, EventReader, FromWorld, Image, Mat3, MaterialPlugin, MouseButton, PerspectiveCameraBundle, PerspectiveProjection, SpawnSceneCommands, Vec2, Vec4, Windows, World};
+use bevy::prelude::{Assets, Camera, EventReader, Mat3, MaterialPlugin, MouseButton, PerspectiveCameraBundle, PerspectiveProjection, State, SystemSet, Vec2, Vec4, Windows, World};
 use bevy::prelude::AssetServer;
 use bevy::prelude::Color;
 use bevy::prelude::Commands;
-use bevy::prelude::Deref;
 use bevy::prelude::Handle;
 use bevy::prelude::Input;
 use bevy::prelude::KeyCode;
@@ -28,22 +27,118 @@ use bevy::prelude::shape;
 use bevy::prelude::Time;
 use bevy::prelude::Transform;
 use bevy::prelude::Vec3;
-use bevy::prelude::With;
 use bevy::reflect::TypeUuid;
 use bevy::render::mesh::MeshVertexBufferLayout;
 use bevy::render::render_asset::{PrepareAssetError, RenderAsset, RenderAssets};
-use bevy::render::render_resource::{BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayout, BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingResource, BindingType, Buffer, BufferBindingType, BufferInitDescriptor, BufferSize, BufferUsages, CachedComputePipelineId, ComputePipelineDescriptor, Extent3d, PipelineCache, RenderPipelineDescriptor, ShaderStages, SpecializedMeshPipelineError, StorageTextureAccess, TextureDimension, TextureFormat, TextureUsages, TextureViewDimension};
+use bevy::render::render_resource::{BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayout, BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingType, Buffer, BufferBindingType, BufferInitDescriptor, BufferSize, BufferUsages, Extent3d, PipelineCache, RenderPipelineDescriptor, ShaderStages, SpecializedMeshPipelineError, StorageTextureAccess, TextureDimension, TextureFormat, TextureUsages, TextureViewDimension};
 use bevy::render::render_resource::std140::{AsStd140, Std140};
 use bevy::render::renderer::{RenderDevice, RenderQueue};
 use bevy::render::{RenderApp, RenderStage};
 use bevy::utils::default;
+use bevy_renet::renet::{ClientAuthentication, RenetClient, RenetConnectionConfig, RenetServer, ServerAuthentication, ServerConfig, ServerEvent};
+use bevy_renet::{RenetClientPlugin, RenetServerPlugin};
 use rand::Rng;
+use bevy::ecs::schedule::ShouldRun;
+
+#[derive(Debug, PartialEq, Eq)]
+enum MultiplayerKind {
+    Client,
+    Host,
+    Local,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum ServerState {
+    Running,
+    Stopping,
+    Stopped
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum ClientState {
+    Running,
+    Stopping,
+    Stopped
+}
+
+fn run_if_client(mode: Res<MultiplayerKind>) -> ShouldRun {
+    if *mode == MultiplayerKind::Client {
+        ShouldRun::Yes
+    } else {
+        ShouldRun::No
+    }
+}
+
+fn run_if_host_or_local(mode: Res<MultiplayerKind>) -> ShouldRun {
+    if *mode == MultiplayerKind::Host || *mode == MultiplayerKind::Local {
+        ShouldRun::Yes
+    } else {
+        ShouldRun::No
+    }
+}
+
+fn run_if_host(mode: Res<MultiplayerKind>) -> ShouldRun {
+    if *mode == MultiplayerKind::Host {
+        ShouldRun::Yes
+    } else {
+        ShouldRun::No
+    }
+}
 
 fn main() {
     let mut app = App::new();
 
+    app.add_plugin(RenetServerPlugin);
+    app.add_plugin(RenetClientPlugin);
+
+    // default to local hosting
+    app.insert_resource(MultiplayerKind::Local);
+
+    app
+        .add_state(ServerState::Stopped)
+        .add_state(ClientState::Stopped);
+
+    // conditionals
+    app.add_system_set(
+        SystemSet::new()
+            .with_run_criteria(run_if_host)
+            .before("input")
+            .with_system(send_message_to_client)
+            .with_system(receive_message_as_server)
+            .with_system(handle_events_as_server)
+    );
+
+    app.add_system_set(
+        SystemSet::new()
+            .with_run_criteria(run_if_client)
+            .before("input")
+            .with_system(send_message_to_server)
+            .with_system(receive_message_as_client)
+    );
+
+    app.add_system_set(
+        SystemSet::on_enter(ClientState::Running)
+            .with_system(prepare_client_socket)
+    );
+
+    app.add_system_set(
+        SystemSet::on_enter(ClientState::Stopping)
+            .with_system(teardown_client_socket)
+    );
+
+    app.add_system_set(
+        SystemSet::on_enter(ServerState::Running)
+            .with_system(host_server)
+    );
+
+    app.add_system_set(
+        SystemSet::on_enter(ServerState::Stopping)
+            .with_system(kill_server)
+    );
+
     // resources
-    app.insert_resource(Msaa { samples: 4 })
+    app
+        .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_plugin(MaterialPlugin::<CustomMaterial>::default())
         .insert_resource(AmbientLight {
@@ -51,40 +146,28 @@ fn main() {
             brightness: 1.0 / 5.0
         })
         .add_startup_system(setup)
-        .add_system(pan_orbit_camera)
-        .add_system(disco)
-        .add_system(light_orbit);
+        .add_startup_system(spawn_player_on_server);
+
+    // input system
+    app.add_system_set(
+        SystemSet::new()
+            .label("input")
+            .with_system(pan_orbit_camera)
+            .with_system(move_torus)
+            .with_system(enter_multiplayer)
+    );
 
     // render stages
     app.sub_app_mut(RenderApp)
-        .init_resource::<VolumetricFogPipeline>()
         .add_system_to_stage(RenderStage::Extract, extract_camera_eye)
-        .add_system_to_stage(RenderStage::Prepare, prepare_camera_eye)
-        .add_system_to_stage(RenderStage::Extract, extract_volume_texture)
-        .add_system_to_stage(RenderStage::Queue, queue_bind_group);
+        .add_system_to_stage(RenderStage::Prepare, prepare_camera_eye);
 
     app.run();
 }
 
 fn setup(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<CustomMaterial>>,
-    mut images: ResMut<Assets<Image>>
 ) {
-
-    // torus
-    commands.spawn().insert_bundle(MaterialMeshBundle {
-        mesh: meshes.add(Mesh::from(shape::Torus { radius: 2.0, ring_radius: 1.0, subdivisions_segments: 24, subdivisions_sides: 64 })),
-        material: materials.add(CustomMaterial{
-            color: Color::rgb(1.0, 0.0, 0.0),
-            enabled: true,
-            ..default()
-        }),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
-
     commands.spawn_bundle(PerspectiveCameraBundle {
         transform: Transform::from_xyz(-10.0, 5.0, -10.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
@@ -102,112 +185,197 @@ fn setup(
         transform: Transform::from_xyz(0.0, 10.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
-
-    let mut image = Image::new_fill(
-        Extent3d {
-            width: 160,
-            height: 90,
-            depth_or_array_layers: 1
-        },
-        TextureDimension::D2,
-        &[0, 0, 0, 0, 0, 0, 0, 0],
-        TextureFormat::Rgba16Float
-    );
-    image.texture_descriptor.usage = TextureUsages::COPY_DST | TextureUsages::STORAGE_BINDING | TextureUsages::TEXTURE_BINDING;
-    let image = images.add(image);
-    commands.insert_resource(TextureVolume(image));
 }
 
-fn light_orbit(time: Res<Time>, mut query: Query<&mut Transform, With<PointLight>>) {
-    for mut lightb in query.iter_mut() {
-        lightb.rotate_around(Vec3::ZERO, Quat::from_rotation_z(PI * time.delta_seconds()));
+fn spawn_player_on_server(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<CustomMaterial>>,mode: Res<MultiplayerKind>) {
+    if *mode == MultiplayerKind::Host || *mode == MultiplayerKind::Local {
+        commands.spawn().insert_bundle(MaterialMeshBundle {
+            mesh: meshes.add(Mesh::from(shape::Torus { radius: 2.0, ring_radius: 1.0, subdivisions_segments: 24, subdivisions_sides: 64 })),
+            material: materials.add(CustomMaterial{
+                color: Color::rgb(1.0, 0.0, 0.0),
+                enabled: true,
+                ..default()
+            }),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        }).insert(Player {});
     }
 }
 
-fn disco(input: Res<Input<KeyCode>>, mut query: Query<&mut PointLight>) {
-    if input.just_pressed(KeyCode::M) {
-        for mut light in query.iter_mut() {
-            let mut rng = rand::thread_rng();
+#[derive(Component)]
+struct Player {}
 
-            light.color = Color::rgb(rng.gen::<f32>(), rng.gen::<f32>(), rng.gen::<f32>());
+fn host_server(mut commands: Commands) {
+    println!("Starting server...");
+    let addr = "127.0.0.1:5000".parse().unwrap();
+    let server = RenetServer::new(
+        SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap(),
+        ServerConfig::new(4, 0, addr, ServerAuthentication::Unsecure),
+        RenetConnectionConfig {
+            // this can be heavily customized
+            ..Default::default()
+        },
+        UdpSocket::bind(addr).unwrap(),
+    ).unwrap();
+    commands.insert_resource(server);
+}
+
+fn kill_server(mut server: ResMut<RenetServer>, mut commands: Commands, mut server_state: ResMut<State<ServerState>>) {
+    println!("Stopping server...");
+    server.disconnect_clients();
+    commands.remove_resource::<RenetServer>();
+    if *server_state.current() != ServerState::Stopped {
+        server_state.set(ServerState::Stopped).unwrap();
+    }
+}
+
+fn prepare_client_socket(mut commands: Commands) {
+    println!("Establishing client socket...");
+    let mut rng = rand::thread_rng();
+    let addr = "127.0.0.1:5000".parse().unwrap();
+    let client_id = rng.gen::<u64>();
+    let client = RenetClient::new(
+        SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap(),
+        UdpSocket::bind("127.0.0.1:0").unwrap(),
+        client_id,
+        RenetConnectionConfig {
+            // this can be heavily customized
+            ..Default::default()
+        },
+        ClientAuthentication::Unsecure {
+            protocol_id: 0,
+            client_id,
+            server_addr: addr,
+            user_data: None
+        }
+    ).unwrap();
+    commands.insert_resource(client);
+}
+
+fn teardown_client_socket(mut client: ResMut<RenetClient>, mut commands: Commands, mut client_state: ResMut<State<ClientState>>) {
+    println!("Tearing down client socket...");
+    client.disconnect();
+    commands.remove_resource::<RenetClient>();
+    if *client_state.current() != ClientState::Stopped {
+        client_state.set(ClientState::Stopped).unwrap();
+    }
+}
+
+fn send_message_to_server(mut client: ResMut<RenetClient>) {
+    let channel_id = 0;
+    client.send_message(channel_id, "hello, server!".as_bytes().to_vec());
+}
+
+fn send_message_to_client(mut server: ResMut<RenetServer>) {
+    let channel_id = 0;
+    server.broadcast_message(channel_id, "hello, client!".as_bytes().to_vec());
+}
+
+fn receive_message_as_client(mut client: ResMut<RenetClient>) {
+    let channel_id = 0;
+    while let Some(message) = client.receive_message(channel_id) {
+        let message: String = String::from_utf8_lossy(message.as_slice()).into_owned();
+        println!("Client got message: {}", message);
+    }
+}
+
+fn receive_message_as_server(mut server: ResMut<RenetServer>) {
+    let channel_id = 0;
+    // Send a text message for all clients
+    for client_id in server.clients_id().into_iter() {
+        while let Some(message) = server.receive_message(client_id, channel_id) {
+            let message: String = String::from_utf8_lossy(message.as_slice()).into_owned();
+            println!("Server got message: {}", message);
         }
     }
 }
 
-#[derive(Deref)]
-struct TextureVolume(Handle<Image>);
-struct TextureVolumeBindGroup(BindGroup);
+fn handle_events_as_server(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<CustomMaterial>>,
+    mut server_events: EventReader<ServerEvent>)
+{
+    for event in server_events.iter() {
+        match event {
+            ServerEvent::ClientConnected(id, _) => {
+                println!("Client {} connected", id);
 
-// extract the volume texture into a resource in render world
-fn extract_volume_texture(mut commands: Commands, image: Res<TextureVolume>) {
-    commands.insert_resource(TextureVolume(image.clone()));
+                // spawn player on the server
+                commands.spawn().insert_bundle(MaterialMeshBundle {
+                    mesh: meshes.add(Mesh::from(shape::Torus { radius: 2.0, ring_radius: 1.0, subdivisions_segments: 24, subdivisions_sides: 64 })),
+                    material: materials.add(CustomMaterial{
+                        color: Color::rgb(1.0, 0.0, 0.0),
+                        enabled: true,
+                        ..default()
+                    }),
+                    transform: Transform::from_xyz(0.0, 0.5, 0.0),
+                    ..default()
+                }).insert(Player {});
+
+                // send message to player to create itself
+            }
+
+            ServerEvent::ClientDisconnected
+            (id) => {
+                println!("Client {} disconnected", id);
+            }
+        }
+    }
 }
 
-fn queue_bind_group(mut commands: Commands,
-    pipeline: Res<VolumetricFogPipeline>,
-    gpu_images: Res<RenderAssets<Image>>,
-    volumetric_texture: Res<TextureVolume>,
-    render_device: Res<RenderDevice>
-) {
-    let view = &gpu_images[&volumetric_texture.0];
-    let bind_group = render_device.create_bind_group(&BindGroupDescriptor {
-        label: None,
-        layout: &pipeline.texture_bind_group_layout,
-        entries: &[BindGroupEntry {
-            binding: 0,
-            resource: BindingResource::TextureView(&view.texture_view),
-        }]
-    });
-    commands.insert_resource(TextureVolumeBindGroup(bind_group));
+fn enter_multiplayer(
+    mut commands: Commands, input: Res<Input<KeyCode>>, mut server_state: ResMut<State<ServerState>>, mut client_state: ResMut<State<ClientState>>) {
+    if input.just_pressed(KeyCode::F4) {
+        println!("Entering HOST mode");
+        if *client_state.current() != ClientState::Stopped && *client_state.current() != ClientState::Stopping {
+            client_state.set(ClientState::Stopping).unwrap();
+        }
+        if *server_state.current() != ServerState::Running {
+            server_state.set(ServerState::Running).unwrap();
+        }
+        commands.insert_resource(MultiplayerKind::Host);
+    }
+
+    if input.just_pressed(KeyCode::F5) {
+        println!("Entering LOCAL mode");
+        if *client_state.current() != ClientState::Stopped && *client_state.current() != ClientState::Stopping {
+            client_state.set(ClientState::Stopping).unwrap();
+        }
+        if *server_state.current() != ServerState::Stopped && *server_state.current() != ServerState::Stopping {
+            server_state.set(ServerState::Stopping).unwrap();
+        }
+        commands.insert_resource(MultiplayerKind::Local);
+    }
+
+    if input.just_pressed(KeyCode::F6) {
+        println!("Entering CLIENT mode");
+        if *server_state.current() != ServerState::Stopped && *server_state.current() != ServerState::Stopping {
+            server_state.set(ServerState::Stopping).unwrap();
+        }
+        if *client_state.current() != ClientState::Running {
+            client_state.set(ClientState::Running).unwrap();
+        }
+        commands.insert_resource(MultiplayerKind::Client);
+    }
 }
 
-pub struct VolumetricFogPipeline {
-    texture_bind_group_layout: BindGroupLayout,
-    init_pipeline: CachedComputePipelineId,
-    update_pipeline: CachedComputePipelineId
-}
+fn move_torus(time: Res<Time>, input: Res<Input<KeyCode>>, mut query: Query<(&Player, &mut Transform)>) {
+    for (_, mut transform) in query.iter_mut() {
+        if input.pressed(KeyCode::W) {
+            transform.translation.z += (3.0 * time.delta_seconds());
+        }
 
-impl FromWorld for VolumetricFogPipeline {
-    fn from_world(world: &mut World) -> Self {
-        let texture_bind_group_layout =
-            world
-                .resource::<RenderDevice>()
-                .create_bind_group_layout(&BindGroupLayoutDescriptor {
-                    label: None,
-                    entries: &[BindGroupLayoutEntry {
-                        binding: 0,
-                        visibility: ShaderStages::COMPUTE,
-                        ty: BindingType::StorageTexture {
-                            access: StorageTextureAccess::ReadWrite,
-                            format: TextureFormat::Rgba16Float,
-                            view_dimension: TextureViewDimension::D2,
-                        },
-                        count: None,
-                    }],
-                });
-        let shader = world
-            .resource::<AssetServer>()
-            .load("shaders/volumetric_fog.wgsl");
-        let mut pipeline_cache = world.resource_mut::<PipelineCache>();
-        let init_pipeline = pipeline_cache.queue_compute_pipeline(ComputePipelineDescriptor {
-            label: None,
-            layout: Some(vec![texture_bind_group_layout.clone()]),
-            shader: shader.clone(),
-            shader_defs: vec![],
-            entry_point: Cow::from("init"),
-        });
-        let update_pipeline = pipeline_cache.queue_compute_pipeline(ComputePipelineDescriptor {
-            label: None,
-            layout: Some(vec![texture_bind_group_layout.clone()]),
-            shader,
-            shader_defs: vec![],
-            entry_point: Cow::from("update"),
-        });
+        if input.pressed(KeyCode::S) {
+            transform.translation.z -= (3.0 * time.delta_seconds());
+        }
 
-        VolumetricFogPipeline {
-            texture_bind_group_layout,
-            init_pipeline,
-            update_pipeline,
+        if input.pressed(KeyCode::A) {
+            transform.translation.x -= (3.0 * time.delta_seconds());
+        }
+
+        if input.pressed(KeyCode::D) {
+            transform.translation.x += (3.0 * time.delta_seconds());
         }
     }
 }
